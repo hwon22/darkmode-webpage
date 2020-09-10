55763221d77456867056bd7a9279c694ea3a834f
HWOWON SEO <59409320+hwon22@users.noreply.github.com>
2020-09-10 23:19:41 +0900

    [최종] 주야간 모드 홈페이지

--- colors.js
+++ colors.js
@@ -20,16 +20,16 @@ setBackgroundColor:function(color){
 }
 }
 function nightDayHandler(self){
-if(self.value==='night'){
+if(self.value==='night-mode'){
   Body.setBackgroundColor('black');
   Body.setColor('white');
-  self.value='day';
+  self.value='day-mode';
   Links.setColor('powderblue');
 }
 else{
   Body.setBackgroundColor('white');
   Body.setColor('black');
-  self.value='night';
-  Links.setColor('blue');
+  self.value='night-mode';
+  Links.setColor('black');
 }
 }
